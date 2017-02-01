ace4513aeaecf730db55a1c6771be4dde11dc882
vanstock <vanstock@ya.ru>
2017-02-01 14:49:16 +0300

    Добавил пробел между числом и единицей времени.

--- timeLabels.js
+++ timeLabels.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name         Время надписей в виде часов и минут. Только на русском языке.
+// @name         Отображение длительности висения «надписей» в форме часов и минут. 
 // @namespace    idLast.com
-// @version      0.2
-// @description  Делает отображение времени надписей на idLast более понятным
+// @version      1.0
+// @description  Делает отображение длительности висения на idLast более понятным. Только на русском языке.
 // @author       vanstock | vanstock.lj.com
 // @include      *idlast.com/*
 // ==/UserScript==
 
+// Костыли, они такие костыли ;)
 (function() {
     // ловлю элемент в котором сидят надписи, мне будут нужны пять первых дивов
     var leftside_divs = document.querySelector('.float_leftside_block').getElementsByTagName('div');
@@ -16,13 +17,13 @@
         var str = leftside_divs[i].innerHTML;
         while(str.search('  ')>-1){str=str.replace('  ',' ');}
 
-        // время занудно ракладываю по частям, чтобы иметь возможность исключать нулевые варианты
+        // занудно ракладываю время по частям, чтобы исключать нулевые варианты
         var old_time = str.split(' ')[3];
-        var hours = Math.floor(old_time/60) + "ч.";
-        var minutes = old_time%60 + "м.";
-        if (hours == '0ч.') hours = '';
-        if (minutes == '0м.') minutes = '';
-        var new_time = hours + ' ' + minutes+'*';
+        var hours = Math.floor(old_time/60) + " ч.";
+        var minutes = old_time%60 + " м.";
+        if (hours == '0 ч.') hours = '';
+        if (minutes == '0 м.') minutes = '';
+        var new_time = hours + ' ' + minutes;
 
         // теперь заменяю старое значение на новое: делаю элемент, вставляю новый перед старым, удаляю старый (его индекс сдивнется)
         var change = document.createElement("div");
